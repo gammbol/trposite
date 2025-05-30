1b22ede8d3716162800e50ba19cfc9e204116e3b
Leonid Ushakov <gammbol2525@gmail.com>
2025-05-30 13:26:08 +0300

    init2

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-history = []
-
-def add_to_history(equation, steps, solution):
-    history.append({
-        'equation': equation,
-        'steps': steps,
-        'solution': solution
-    })
-
---
+++ models.py
@@ -0,0 +1,9 @@
+history = []
+
+def add_to_history(equation, steps, solution):
+    history.append({
+        'equation': equation,
+        'steps': steps,
+        'solution': solution
+    })
+
