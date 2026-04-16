398c6d2ceb62755e08131c4f53d2b5fd3db11259
tryingtothink <gammbol2525@gmail.com>
2026-04-16 21:14:25 +0300

    fixed cors issues

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'solver',
     'history',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +132,7 @@ TEMPLATES[0]['DIRS'] = [
 
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
